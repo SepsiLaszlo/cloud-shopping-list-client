73c8403639f8fd3220d4c7b154e5f87e93f95e82
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-10-02 09:43:04 +0200

    add react router

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,30 @@ import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const container = document.getElementById("root")
 if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container)
 
+
+
 root.render(
   <React.StrictMode>
     <ColorModeScript />
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
